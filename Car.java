5a928108bb05ddb676ecdac002385daf62e43888
KimYouHwan <voddl1664@gmail.com>
2025-03-24 11:57:33 +0900

    Car print 문 추가

--- Car.java
+++ Car.java
@@ -1,5 +1,8 @@
 public class Car {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Car");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
     }
 }
